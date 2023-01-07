17f22f80ff9f5f763fdd3a7623f23b06abc35f10
Jacob Yoon <jacobyoon926@gmail.com>
2023-01-07 13:49:25 -0500

    Homepage redesign

--- ui/src/app/components/home/home.component.ts
+++ ui/src/app/components/home/home.component.ts
@@ -24,7 +24,7 @@ export class HomeComponent {
     getUsers(): void {
         this.userService.getUsers().subscribe(users => {
             this.users = users;
-            this.users.sort(() => Math.random() - 0.5);
+            // this.users.sort(() => Math.random() - 0.5);
             this.getImages();
         });
     }
@@ -58,5 +58,39 @@ export class HomeComponent {
         if (filename && this.images.has(filename))
             return this.images.get(filename);
         return "";
+    }
+
+    random(): number {
+        return Math.random() * 80 - 40;
+    }
+}
+
+const images = document.getElementsByClassName("model-image");
+let globalIndex = 0, last = { x: 0, y: 0 };
+const activate = (image: HTMLElement, x: number, y: number) => {
+    if (image) {
+        image.style.left = `${x}px`;
+        image.style.top = `${y}px`;
+        image.style.zIndex = "" + globalIndex;
+        image.classList.add("active");
+        last = {x,y};
+    }
+}
+const deactivate = (image: HTMLElement) => {
+    if (image) {
+        image.classList.remove("active");
+    }
+}
+const distanceFromLast = (x: number, y: number) => {
+    return Math.hypot(x - last.x, y - last.y);
+}
+window.onmousemove = e => {
+    if (distanceFromLast(e.clientX, e.clientY) > (window.innerWidth / 20)) {
+        const lead = images[globalIndex % images.length],
+                tail = images[(globalIndex - Math.min(10, images.length)) % images.length];
+        activate(lead as HTMLElement, e.clientX, e.clientY);
+        // deactivate(tail as HTMLElement);
+        
+        globalIndex++;
     }
 }
